182d6efd745f8a105339748e172cadda69cf7564
Julián Rosalen <rosalenjulian@gmail.com>
2020-12-10 15:49:24 -0300

    Move function for check year to utils.js

--- src/data/data-races.ts
+++ src/data/data-races.ts
@@ -1,3 +1,4 @@
+import { yearHandler } from '../lib/utils';
 import { F1 } from './data-source';
 
 export class RacesData extends F1 {
@@ -7,9 +8,7 @@ export class RacesData extends F1 {
 
 // Access to seasons in API REST, only need the path. Base path URL already exists in data-source.ts (F1 class)
   async getYear(year:string) {
-      const currentYear = new Date().getFullYear();
-      // if the value of year isn't valid set currentYear
-      (isNaN(+year) || +year < 1950 || +year > currentYear) ? year = String(currentYear) : year = year ;
+     year = yearHandler(year)
       return await this.get(`${year}.json`, {
           //Add cache for an hour
           cacheOptions: { ttl:60 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,12 @@
-export function getWikipediaMobileUrl(url:string){
-    return (url !== undefined) ? url.replace('wikipedia', 'm.wikipedia') : "";
+export function getWikipediaMobileUrl(url: string) {
+  return url !== undefined ? url.replace('wikipedia', 'm.wikipedia') : '';
+}
+
+export function yearHandler(year: string) {
+  const currentYear = new Date().getFullYear();
+  // if the value of year isn't valid set currentYear
+  if (isNaN(+year) || +year < 1950 || +year > currentYear) {
+    year = String(currentYear);
+  }
+  return year;
 }
