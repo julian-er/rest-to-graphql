e3b0bfbfb3ff57b8b4707fcf5d93b1f5c371c44b
Julián Rosalen <rosalenjulian@gmail.com>
2020-12-10 11:07:45 -0300

    Update urlMobile with function

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -1,11 +1,12 @@
 import { IResolvers } from 'graphql-tools';
+import { getWikipediaMobileUrl } from '../lib/utils';
 
 const type: IResolvers = {
   Season:{
     //resolve names, in API REST, is a season. Here I want to name this with year word
       year: parent => parent.season,
-    //resolve urlMobile with url mobile
-      urlMobile: parent => parent.url
+    //resolve urlMobile with url mobile importing function from utils
+      urlMobile: parent => getWikipediaMobileUrl(parent.url)
   }
 };
 
