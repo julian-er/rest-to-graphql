0bbc7918e1bd9399b6f1722bb2720484e79cda91
Julián Rosalen <rosalenjulian@gmail.com>
2020-12-10 14:50:27 -0300

    Adding Race correction types

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -7,6 +7,12 @@ const type: IResolvers = {
       year: parent => parent.season,
     //resolve urlMobile with url mobile importing function from utils
       urlMobile: parent => getWikipediaMobileUrl(parent.url)
+  },
+  Race:{
+    //resolve names, in API REST, is a raceName. Here I want to name this with name word
+    name: parent => parent.raceName,
+    //resolve names, in API REST, is a Circuit. Here I want to name this with circuit word
+    circuit: parent => parent.Circuit
   }
 };
 
