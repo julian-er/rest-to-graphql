f3f58214fdbdc28ceb0f5a6c1201d4a73a76d452
Julián Rosalen <rosalenjulian@gmail.com>
2020-12-10 10:47:08 -0300

    Query for obtain Seasons

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,9 +1,17 @@
 import { IResolvers } from 'graphql-tools';
 
 const resolvers: IResolvers = {
-    Query: {
-     
-    }
+  Query: {
+    //dataSources from server.ts , acces to declared seasons and then to function defined in data-seasons.ts
+    async seasonsList(_: void, __: any, { dataSources }) {
+      return await dataSources.seasons.getSeasons().then(
+        // this structure came from data in API REST
+        (data: any) => {
+          return data.MRData.SeasonTable.Seasons;
+        }
+      );
+    },
+  },
 };
 
 export default resolvers;
