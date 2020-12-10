2636c27cade68d7dca1a003446b4c7609b08aaa0
Julián Rosalen <rosalenjulian@gmail.com>
2020-12-10 10:51:36 -0300

    Fix query name for obtain Seasons

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -3,7 +3,7 @@ import { IResolvers } from 'graphql-tools';
 const resolvers: IResolvers = {
   Query: {
     //dataSources from server.ts , acces to declared seasons and then to function defined in data-seasons.ts
-    async seasonsList(_: void, __: any, { dataSources }) {
+    async SeasonsList(_: void, __: any, { dataSources }) {
       return await dataSources.seasons.getSeasons().then(
         // this structure came from data in API REST
         (data: any) => {
