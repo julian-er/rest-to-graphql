fb8ec224bb1eb8277476718f80ca429c48c916e9
Julián Rosalen <rosalenjulian@gmail.com>
2020-12-10 17:06:07 -0300

    Add urlMobile to Race and circuit

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -12,7 +12,9 @@ const type: IResolvers = {
     //resolve names, in API REST, is a raceName. Here I want to name this with name word
     name: parent => parent.raceName,
     //resolve names, in API REST, is a Circuit. Here I want to name this with circuit word
-    circuit: parent => parent.Circuit
+    circuit: parent => parent.Circuit,
+    //resolve urlMobile with url mobile importing function from utils
+    urlMobile: parent => getWikipediaMobileUrl(parent.url)
   },
   Circuit:{
     //resolve names, in API REST, is a circuitId. Here I want to name this with id word
@@ -20,7 +22,9 @@ const type: IResolvers = {
     //resolve names, in API REST, is a circuitName. Here I want to name this with name word    
     name: parent => parent.circuitName,
     //resolve names, in API REST, is a Location. Here I want to name this with location word
-    location: parent=> parent.Location
+    location: parent=> parent.Location,
+    //resolve urlMobile with url mobile importing function from utils
+    urlMobile: parent => getWikipediaMobileUrl(parent.url)
   }
 };
 
